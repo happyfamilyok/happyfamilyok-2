291f8551262831cb3a56bffd2ed4b46617943dac
indiiskies <jonathanzheng135@gmail.com>
2025-04-28 23:52:15 -0400

    Add hover rotate animation to URL frame using Tailwind CSS & change view all menu to jump to kids menu instead of showing notimplementederror

--- script.js
+++ script.js
@@ -2,3 +2,6 @@ function loadURL() {
     var url = document.getElementById('urlInput').value;
     document.getElementById('urlFrame').src = url;
 }
+
+// Add Tailwind hover rotate 90 animation
+document.getElementById('urlFrame').className = 'transition-transform duration-500 hover:rotate-90';
