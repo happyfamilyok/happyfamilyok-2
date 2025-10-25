0283b2bb55c6e54331bbc3f17b0fb34396f7795f
Jonathan Zheng <jonathanzheng135@gmail.com>
2025-10-24 23:44:48 -0400

    Update nuxt.config.ts

--- app2/nuxt.config.ts
+++ app2/nuxt.config.ts
@@ -6,7 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-    css: [
+  css: [
     '~/assets/css/main.css',
     'animate.css/animate.min.css',
     '@fortawesome/fontawesome-free/css/all.min.css'
@@ -17,15 +17,32 @@ export default defineNuxtConfig({
       meta: [
         { 
           name: 'Content-Security-Policy', 
-          content: "default-src 'self' https:; connect-src 'self' https: https://www.googletagmanager.com https://www.google-analytics.com https://static.hotjar.com https://in.hotjar.com https://script.hotjar.com wss://ws.hotjar.com; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://cdn.apple-mapkit.com https://www.googletagmanager.com https://static.hotjar.com https://script.hotjar.com; style-src 'self' 'unsafe-inline' https: https://static.hotjar.com; img-src 'self' data: https: http: https://www.google-analytics.com https://www.googletagmanager.com https://static.hotjar.com https://in.hotjar.com; font-src 'self' https: data: https://static.hotjar.com; frame-src https://static.hotjar.com;" 
+          content: `
+            default-src 'self' https:;
+            connect-src 'self' https: https://www.googletagmanager.com https://www.google-analytics.com https://static.hotjar.com https://in.hotjar.com https://script.hotjar.com wss://ws.hotjar.com;
+            script-src 'self' 'unsafe-inline' 'unsafe-eval' https://cdn.apple-mapkit.com https://www.googletagmanager.com https://static.hotjar.com https://script.hotjar.com;
+            style-src 'self' 'unsafe-inline' https: https://static.hotjar.com;
+            img-src 'self' data: https: http: https://www.google-analytics.com https://www.googletagmanager.com https://static.hotjar.com https://in.hotjar.com;
+            font-src 'self' https: data: https://static.hotjar.com;
+            frame-src https://static.hotjar.com;
+          `
         },
         { 
           'http-equiv': 'Content-Security-Policy', 
-          content: "default-src 'self' https:; connect-src 'self' https: https://www.googletagmanager.com https://www.google-analytics.com https://static.hotjar.com https://in.hotjar.com https://script.hotjar.com wss://ws.hotjar.com; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://cdn.apple-mapkit.com https://www.googletagmanager.com https://static.hotjar.com https://script.hotjar.com; style-src 'self' 'unsafe-inline' https: https://static.hotjar.com; img-src 'self' data: https: http: https://www.google-analytics.com https://www.googletagmanager.com https://static.hotjar.com https://in.hotjar.com; font-src 'self' https: data: https://static.hotjar.com; frame-src https://static.hotjar.com;" 
+          content: `
+            default-src 'self' https:;
+            connect-src 'self' https: https://www.googletagmanager.com https://www.google-analytics.com https://static.hotjar.com https://in.hotjar.com https://script.hotjar.com wss://ws.hotjar.com;
+            script-src 'self' 'unsafe-inline' 'unsafe-eval' https://cdn.apple-mapkit.com https://www.googletagmanager.com https://static.hotjar.com https://script.hotjar.com;
+            style-src 'self' 'unsafe-inline' https: https://static.hotjar.com;
+            img-src 'self' data: https: http: https://www.google-analytics.com https://www.googletagmanager.com https://static.hotjar.com https://in.hotjar.com;
+            font-src 'self' https: data: https://static.hotjar.com;
+            frame-src https://static.hotjar.com;
+          `
         },
         { name: 'referrer', content: 'no-referrer-when-downgrade' }
       ],
       script: [
+        // --- Combined gtag.js for GA + Ads ---
         {
           async: true,
           src: 'https://www.googletagmanager.com/gtag/js?id=G-PSKN1R7CLW'
@@ -35,15 +52,17 @@ export default defineNuxtConfig({
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
+            // Google Analytics
             gtag('config', 'G-PSKN1R7CLW', {
               page_title: 'Happy Family - SiteV2',
-              custom_map: {
-                'dimension1': 'stream_name'
-              }
+              custom_map: {'dimension1': 'stream_name'}
             });
+            // Google Ads
+            gtag('config', 'AW-606049347');
           `,
           type: 'text/javascript'
         },
+        // Hotjar
         {
           innerHTML: `
             (function(h,o,t,j,a,r){
@@ -70,7 +89,15 @@ export default defineNuxtConfig({
     routeRules: {
       '/**': {
         headers: {
-          'Content-Security-Policy': "default-src 'self' https:; connect-src 'self' https: https://www.googletagmanager.com https://www.google-analytics.com https://static.hotjar.com https://in.hotjar.com https://script.hotjar.com wss://ws.hotjar.com; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://cdn.apple-mapkit.com https://www.googletagmanager.com https://static.hotjar.com https://script.hotjar.com; style-src 'self' 'unsafe-inline' https: https://static.hotjar.com; img-src 'self' data: https: http: https://www.google-analytics.com https://www.googletagmanager.com https://static.hotjar.com https://in.hotjar.com; font-src 'self' https: data: https://static.hotjar.com; frame-src https://static.hotjar.com;",
+          'Content-Security-Policy': `
+            default-src 'self' https:;
+            connect-src 'self' https: https://www.googletagmanager.com https://www.google-analytics.com https://static.hotjar.com https://in.hotjar.com https://script.hotjar.com wss://ws.hotjar.com;
+            script-src 'self' 'unsafe-inline' 'unsafe-eval' https://cdn.apple-mapkit.com https://www.googletagmanager.com https://static.hotjar.com https://script.hotjar.com;
+            style-src 'self' 'unsafe-inline' https: https://static.hotjar.com;
+            img-src 'self' data: https: http: https://www.google-analytics.com https://www.googletagmanager.com https://static.hotjar.com https://in.hotjar.com;
+            font-src 'self' https: data: https://static.hotjar.com;
+            frame-src https://static.hotjar.com;
+          `,
           'X-Content-Type-Options': 'nosniff',
           'X-Frame-Options': 'DENY',
           'X-XSS-Protection': '1; mode=block'
